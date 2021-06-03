3fed841059dc5e0899bd767cd086494ddbebc283
fexjac <fexjac@gmail.com>
2021-06-03 14:32:15 -0400

    Api base

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, productName, productDescription, productPrice) {
+        this.id = id;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+    }
+}
+
+module.exports = Product;
